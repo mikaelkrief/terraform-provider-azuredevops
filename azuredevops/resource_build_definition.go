c5d49ad91ff3d5b602ff6a15336b0558fc7312a6
Keith Rome <keith.rome@gmail.com>
2019-10-17 12:51:41 -0500

    Build Def resource only supports github right now

--- azuredevops/resource_build_definition.go
+++ azuredevops/resource_build_definition.go
@@ -57,7 +57,7 @@ func resourceBuildDefinition() *schema.Resource {
 						"repo_type": {
 							Type:         schema.TypeString,
 							Required:     true,
-							ValidateFunc: validation.StringInSlice([]string{"GitHub","AzDevOps"}, false),
+							ValidateFunc: validation.StringInSlice([]string{"GitHub"}, false),
 						},
 						"branch_name": {
 							Type:     schema.TypeString,
